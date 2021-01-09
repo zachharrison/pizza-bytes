ea5c52a79431894e8df4d8b6602bb5f55c29aa00
Natasha Colusso <natashacolusso@gmail.com>
2021-01-09 23:30:38 +0000

    Test Data: Adding additional seeds test database

--- db/seeds/allSeeds.sql
+++ db/seeds/allSeeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO customers (name, email, phone_number) 
+INSERT INTO customers (name, email, phone_number)
 VALUES ('Alice', 'alice@gmail.com', '250-580-7961'),
 ('Natasha', 'natasha@gmail.com', '778-877-8963'),
 ('Adam', 'adam.tranquilla@gmail.com', '403-710-5087'),
@@ -9,19 +9,19 @@ VALUES ('Pizza Bytes', 'pizzabytes@gmail.com', 'password', '100-101-0101');
 
 INSERT INTO menu_items (restaurant_id, name, photo_url, price)
 VALUES (
-  1, 
+  1,
   'Build Your Own',
   './public/img/Pizza-Toss.png',
   500
 ),
 (
-  1, 
+  1,
   'Classic Cheese Pizza',
   './public/img/cheese.png',
   1000
 ),
 (
-  1, 
+  1,
   'Pepperoni Pizza',
   './public/img/pepperoni.png',
   1000
@@ -33,7 +33,7 @@ VALUES (
   1000
 ),
 (
-  1, 
+  1,
   'Vegetarian Pizza',
   './public/img/veg.png',
   1000
@@ -45,13 +45,13 @@ VALUES (
   1000
 ),
 (
-  1, 
+  1,
   'Hawaiian Pizza',
   './public/img/hawaiian.png',
   1000
 ),
 (
-  1, 
+  1,
   'BBQ Chicken Pizza',
   './public/img/bbq-chicken.png',
   1000
@@ -63,7 +63,9 @@ VALUES (1, 2, FALSE);
 INSERT INTO orders (restaurant_id, order_date, customer_id, pickup_time, completed)
 VALUES (1, '2021-01-09 19:03:11.242422', 1, '2021-01-09 19:33:11.242422', TRUE),
 (1, '2021-01-08 19:03:11.242422', 1, '2021-01-08 19:33:11.242422', TRUE),
-(1, '2021-01-09 20:03:11.242422', 1, '2021-01-09 20:33:11.242422', FALSE);
+(1, '2021-01-09 20:03:11.242422', 1, '2021-01-09 20:33:11.242422', FALSE),
+(1, '2021-01-06 19:03:11.242422', 3, '2021-01-06 19:33:11.242422', TRUE),
+(1, '2021-01-07 19:03:11.242422', 4, '2021-01-07 19:33:11.242422', TRUE);
 
 INSERT INTO sizes (name, price)
 VALUES ('small', 0),
@@ -72,7 +74,7 @@ VALUES ('small', 0),
 
 INSERT INTO topping_categories (name, price)
 VALUES ('Cheeses', 200),
-('Meats', 150), 
+('Meats', 150),
 ('Veggies', 125);
 
 INSERT INTO toppings (topping_category_id, name)
@@ -96,28 +98,28 @@ VALUES (1, 'Mozzarella'),
 
 INSERT INTO order_items (order_id, size_id, quantity, menu_item_id)
 VALUES (1, 3, 1, 1),
-(1, 2, 1, 2), 
+(1, 2, 1, 2),
 (2, 3, 2, 1),
 (3, 2, 1, 3),
-(3, 2, 1, 4), 
-(4, 1, 3, 1), 
+(3, 2, 1, 4),
+(4, 1, 3, 1),
 (4, 2, 2, 2);
 
 -- 7 order items total to add potential toppings to
 INSERT INTO order_item_toppings (topping_id, order_item_id)
 VALUES (3, 1),
 (14, 1),
-(12, 1), 
-(1, 2), 
-(4, 2), 
-(9, 2), 
+(12, 1),
+(1, 2),
+(4, 2),
+(9, 2),
 (5, 3),
 (9, 3),
-(10, 4), 
-(12, 5), 
-(1, 5), 
-(4, 6), 
-(9, 6), 
+(10, 4),
+(12, 5),
+(1, 5),
+(4, 6),
+(9, 6),
 (5, 6),
 (9, 7),
 (10, 7);
