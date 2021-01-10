deff3ab23b487e22a10b5dee300af65a021c3e29
Natasha Colusso <natashacolusso@gmail.com>
2021-01-10 02:05:29 +0000

    Schema: Correcting missing link between topping/menu items

--- db/schema/06_toppings.sql
+++ db/schema/06_toppings.sql
@@ -3,5 +3,6 @@ DROP TABLE IF EXISTS toppings;
 CREATE TABLE toppings (
     id SERIAL PRIMARY KEY NOT NULL,
     topping_category_id INTEGER REFERENCES topping_categories(id),
-    name VARCHAR(255) NOT NULL
+    name VARCHAR(255) NOT NULL,
+    menu_item_id INTEGER REFERENCES menu_items(id)
 );
