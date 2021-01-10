23ae61f515c4820b00c8845828e79a2b784fbf5b
Natasha Colusso <natashacolusso@gmail.com>
2021-01-10 02:24:58 +0000

    removing schema/seed files since they have been combined to one mastr for each

--- db/schema/02.5_menu_items.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS menu_items CASCADE;
-
-CREATE TABLE menu_items (
-    id SERIAL PRIMARY KEY NOT NULL,
-    restaurant_id INTEGER REFERENCES restaurants(id),
-    name VARCHAR(255) NOT NULL,
-    photo_url VARCHAR(255) NOT NULL,
-    price INTEGER NOT NULL
-);
--- db/seeds/01_customers.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO customers (name, email, phone_number) 
-VALUES ('Alice', 'alice@gmail.com', '250-580-7961'),
-('Natasha', 'natasha@gmail.com', '778-877-8963'),
-('Adam', 'adam.tranquilla@gmail.com', '403-710-5087'),
-('Zach', 'zach@gmail.com', '250-765-4321');
-
