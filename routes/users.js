662159c2b4d05d391410d6aefd91f2747f584436
Natasha Colusso <natashacolusso@gmail.com>
2021-01-10 20:40:24 +0000

    Styling: started basic styling of homepage and resetting server/routing files

--- routes/users.js
+++ routes/users.js
@@ -5,20 +5,19 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users;`)
-      .then(data => {
+    console.log(db);
+    db.query(`SELECT * FROM restaurants;`)
+      .then((data) => {
         const users = data.rows;
         res.json({ users });
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
   });
   return router;
